14cf7d07e1bb08601888c43b116c9010bedeae7c
Temitope <tope_olajide@outlook.com>
2019-11-24 06:20:37 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { StoreProvider } from './Store';
+import './index.css';
+ReactDOM.render(
+    <StoreProvider>
+      <App />
+    </StoreProvider>,
+    document.getElementById('root')
+  );
+
